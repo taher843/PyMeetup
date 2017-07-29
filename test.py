dead0d669994d3d16aa992674406fc5a215947ce
Taher Tamboli <taher843@gmail.com>
2017-07-29 06:53:33 +0530

    This is a new update

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-import sqlite3
-import threading
-
 print ("You're Awesome aren't you ?")
 print ("Statement d")
 print ("Git is interesting")
-Print ("This is still working")
+print ("This is still working")
