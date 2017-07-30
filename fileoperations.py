9daf1466356439713d3cb861c630c4c32998314e
Taher Tamboli <taher843@gmail.com>
2017-07-30 12:55:06 +0530

    File operations and file

---
+++ fileoperations.py
@@ -0,0 +1,7 @@
+file_name = 'file.txt'
+
+with open(file_name,'w') as f:
+    f.write("1. Attend the meetup \n")
+    f.write("2. Go home and get sleep \n")
+    f.write("3. make sure to have lunch before going to bed \n")
+print("File Written successfully")
