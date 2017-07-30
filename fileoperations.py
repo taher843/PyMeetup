f45440ded3d62fc4336b6e649ba5ae3296010f20
Taher Tamboli <taher843@gmail.com>
2017-07-30 13:08:36 +0530

    ss

--- fileoperations.py
+++ fileoperations.py
@@ -1,7 +1,21 @@
 file_name = 'file.txt'
 
-with open(file_name,'w') as f:
-    f.write("1. Attend the meetup \n")
-    f.write("2. Go home and get sleep \n")
-    f.write("3. make sure to have lunch before going to bed \n")
-print("File Written successfully")
+def write():
+    with open(file_name,'w') as f:
+        f.write("1. Attend the meetup \n")
+        f.write("2. Go home and get sleep \n")
+        f.write("3. make sure to have lunch before going to bed \n")
+    print("File Written successfully")
+
+def append():
+    with open(file_name,'a') as d:
+        d.write("2. Go home and get sleep \n")
+        d.write("3. make sure to have lunch before going to bed \n")
+        d.seek(12)
+        d.write("This is after seek")
+    print("File Written successfully")
+
+append()
+
+
+
