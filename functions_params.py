bdc87c78e1e13c422f66c5cc1dbbe6f50ac98a17
Taher Tamboli <taher843@gmail.com>
2017-07-30 12:42:36 +0530

    while and exceptions

--- functions_params.py
+++ functions_params.py
@@ -5,4 +5,4 @@ def print_msg(msg, error="No Error", *args, **kwargs):
     print("Args", args)
     print("Kwargs", kwargs)
 
-print("This will be logged", "File not found", "-13", "-1", "0" ,key_1="1,2,3,4,5", key_2="5,6,7,8")
+print("This will be logged", "File not found", "-13", "-1", "0")# ,key_1="1,2,3,4,5", key_2="5,6,7,8")
